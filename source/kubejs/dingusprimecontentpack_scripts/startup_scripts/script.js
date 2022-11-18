8d895f1888c04f9e706d6fc5493ac079a26b57bf
Peter Cashel <pacas00@gmail.com>
2022-11-18 22:05:24 +1000

    sample scripts

--- source/kubejs/dingusprimecontentpack_scripts/startup_scripts/script.js
+++ source/kubejs/dingusprimecontentpack_scripts/startup_scripts/script.js
@@ -16,6 +16,8 @@ onEvent('block.registry', event => {
 	event.create('tombstone_5_top').material('stone').hardness(1.0).displayName('Decorative Tombstone 5 Top').defaultCutout()
 	event.create('tombstone_7_top').material('stone').hardness(1.0).displayName('Decorative Tombstone 7 Top').defaultCutout()
 	
+	//customcardinal
+	event.create('tombstone_1_new', 'customcardinal').material('stone').hardness(1.0).displayName('Decorative Tombstone 1').defaultCutout()
 })
 
 
@@ -24,9 +26,9 @@ onEvent('item.registry', event => {
 	event.create('petercashel_cashelcoin').displayName('CashelCoin').tooltip("A rare and exceptionally valueless coin. Destroying it would make someone sad though.")
 	
 	
-	//event.create('hamazon_flint', 'gameboy').displayName('hamazon FLINT').tooltip("A rare and exceptionally... wait this has batteries in it...")
-	//event.create('hamazon_flint_red', 'gameboy').displayName('Red hamazon FLINT').tooltip("A rare and exceptionally... wait this has batteries in it...").guiBG('red')
-	//event.create('hamazon_flint_purple', 'gameboy').displayName('Purple hamazon FLINT').tooltip("A rare and exceptionally... wait this has batteries in it...").guiBG('purple')
+	event.create('hamazon_flint', 'gameboy').displayName('hamazon FLINT').tooltip("A rare and exceptionally... wait this has batteries in it...")
+	event.create('hamazon_flint_red', 'gameboy').displayName('Red hamazon FLINT').tooltip("A rare and exceptionally... wait this has batteries in it...").guiBG('red')
+	event.create('hamazon_flint_purple', 'gameboy').displayName('Purple hamazon FLINT').tooltip("A rare and exceptionally... wait this has batteries in it...").guiBG('purple')
 
 	//event.create('testgame', 'gbcart').displayName('TestGame').gameID('testgame').parentModel('kubejs:item/gbcart')
 	//event.create('tetrisdx', 'gbcart').displayName('Tetris DX').gameID('tetrisdx')
@@ -39,4 +41,12 @@ onEvent('rom_registry', event => {
 	//event.create('testgame').romPath('kubejs:rom/testgame.gb')
 	//event.create('tetrisdx').romPath('kubejs:rom/tetrisdx.gbc')
 	//event.create('pkmonred').romPath('kubejs:rom/pkmonred.gbc')
+})
+
+onEvent('shoptrade_registry', event => {
+	// Register new Shop Trades here
+	event.create('tombstone_1').shopType('furniture').result('kubejs:tombstone_1').shopResultType('block').count(3).cost(5)
+	event.create('tombstone_2').shopType('furniture').result('kubejs:tombstone_2').shopResultType('block').count(2).cost(2)
+	event.create('tombstone_3').shopType('furniture').result('kubejs:tombstone_3').shopResultType('block').count(3).cost(6)
+	event.create('cobblestone').shopType('general').result('cobblestone').shopResultType('block').count(64).cost(1).always()
 })
