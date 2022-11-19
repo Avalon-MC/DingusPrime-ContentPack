ca9315e06d66769fe551960ec10ab74602df219f
Peter Cashel <pacas00@gmail.com>
2022-11-19 22:08:57 +1000

    script updates

--- source/kubejs/dingusprimecontentpack_scripts/startup_scripts/script.js
+++ source/kubejs/dingusprimecontentpack_scripts/startup_scripts/script.js
@@ -5,19 +5,19 @@ onEvent('block.registry', event => {
 	
 	
 	//  SAVED ID ->  BLOCK NAME 			 MAT                                                         
-	event.create('tombstone_1').material('stone').hardness(1.0).displayName('Decorative Tombstone 1').defaultCutout()
-	event.create('tombstone_2').material('stone').hardness(1.0).displayName('Decorative Tombstone 2').defaultCutout()
-	event.create('tombstone_3').material('stone').hardness(1.0).displayName('Decorative Tombstone 3').defaultCutout()
-	event.create('tombstone_4_bottom').material('stone').hardness(1.0).displayName('Decorative Tombstone 4').defaultCutout()
-	event.create('tombstone_5_bottom').material('stone').hardness(1.0).displayName('Decorative Tombstone 5').defaultCutout()
-	event.create('tombstone_6').material('stone').hardness(1.0).displayName('Decorative Tombstone 6').defaultCutout()
-	event.create('tombstone_7_bottom').material('stone').hardness(1.0).displayName('Decorative Tombstone 7').defaultCutout()
-	event.create('tombstone_4_top').material('stone').hardness(1.0).displayName('Decorative Tombstone 4 Top').defaultCutout()
-	event.create('tombstone_5_top').material('stone').hardness(1.0).displayName('Decorative Tombstone 5 Top').defaultCutout()
-	event.create('tombstone_7_top').material('stone').hardness(1.0).displayName('Decorative Tombstone 7 Top').defaultCutout()
+	event.create('tombstone_1').material('stone').hardness(1.0).displayName('Decorative Tombstone 1').defaultCutout().box(2, 0, 2, 14, 16, 6, true)
+	event.create('tombstone_2').material('stone').hardness(1.0).displayName('Decorative Tombstone 2').defaultCutout().box(2, 0, 2, 14, 16, 6, true)
+	event.create('tombstone_3').material('stone').hardness(1.0).displayName('Decorative Tombstone 3').defaultCutout().box(2, 0, 2, 14, 16, 6, true)
+	event.create('tombstone_4_bottom').material('stone').hardness(1.0).displayName('Decorative Tombstone 4').defaultCutout().box(1, 0, 1, 15, 4, 15, true).box(4, 0, 4, 12, 16, 12, true)
+	event.create('tombstone_5_bottom').material('stone').hardness(1.0).displayName('Decorative Tombstone 5').defaultCutout().box(1, 0, 1, 15, 4, 15, true).box(4, 0, 4, 12, 16, 12, true)
+	event.create('tombstone_6').material('stone').hardness(1.0).displayName('Decorative Tombstone 6').defaultCutout().box(2, 0, 2, 14, 16, 6, true)
+	event.create('tombstone_7_bottom').material('stone').hardness(1.0).displayName('Decorative Tombstone 7').defaultCutout().box(1, 0, 1, 15, 4, 15, true).box(4, 0, 4, 12, 16, 12,  true)
+	event.create('tombstone_4_top').material('stone').hardness(1.0).displayName('Decorative Tombstone 4 Top').defaultCutout().box(2, 0, 5, 14, 9, 11, true)
+	event.create('tombstone_5_top').material('stone').hardness(1.0).displayName('Decorative Tombstone 5 Top').defaultCutout().box(2, 0, 5, 14, 9, 11, true)
+	event.create('tombstone_7_top').material('stone').hardness(1.0).displayName('Decorative Tombstone 7 Top').defaultCutout().box(2, 0, 5, 14, 9, 11, true)
 	
 	//customcardinal
-	event.create('tombstone_1_new', 'customcardinal').material('stone').hardness(1.0).displayName('Decorative Tombstone 1').defaultCutout()
+	event.create('tombstone_1_new', 'customcardinal').material('stone').hardness(1.0).displayName('Decorative Tombstone 1').defaultCutout().box(2, 0, 10, 14, 16, 14, true)
 })
 
 
@@ -26,9 +26,11 @@ onEvent('item.registry', event => {
 	event.create('petercashel_cashelcoin').displayName('CashelCoin').tooltip("A rare and exceptionally valueless coin. Destroying it would make someone sad though.")
 	
 	
-	event.create('hamazon_flint', 'gameboy').displayName('hamazon FLINT').tooltip("A rare and exceptionally... wait this has batteries in it...")
-	event.create('hamazon_flint_red', 'gameboy').displayName('Red hamazon FLINT').tooltip("A rare and exceptionally... wait this has batteries in it...").guiBG('red')
-	event.create('hamazon_flint_purple', 'gameboy').displayName('Purple hamazon FLINT').tooltip("A rare and exceptionally... wait this has batteries in it...").guiBG('purple')
+	event.create('gameboy_grey', 'gameboy').displayName('Grey hamazon FLINT').guiBG('default').tooltip("A handheld game player. Takes carts, Volume adjustable via the 'Voice\/Speech' slider")
+	event.create('gameboy_red', 'gameboy').displayName('Red hamazon FLINT').guiBG('red').tooltip("A handheld game player. Takes carts, Volume adjustable via the 'Voice\/Speech' slider")
+	event.create('gameboy_red_clear', 'gameboy').displayName('Red hamazon FLINT').guiBG('red_clear').tooltip("A handheld game player. Takes carts, Volume adjustable via the 'Voice\/Speech' slider")
+	event.create('gameboy_purple', 'gameboy').displayName('Purple hamazon FLINT').guiBG('purple').tooltip("A handheld game player. Takes carts, Volume adjustable via the 'Voice\/Speech' slider")
+	event.create('gameboy_purple_clear', 'gameboy').displayName('Purple hamazon FLINT').guiBG('purple_clear').tooltip("A handheld game player. Takes carts, Volume adjustable via the 'Voice\/Speech' slider")
 
 	//event.create('testgame', 'gbcart').displayName('TestGame').gameID('testgame').parentModel('kubejs:item/gbcart')
 	//event.create('tetrisdx', 'gbcart').displayName('Tetris DX').gameID('tetrisdx')
