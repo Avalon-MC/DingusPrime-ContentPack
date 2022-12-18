e9174c2a2800c1f2fb25be640f3c83684423e6c7
Peter Cashel <pacas00@gmail.com>
2022-12-18 16:27:15 +1000

    fix

--- source/kubejs/dingusprimecontentpack_scripts/startup_scripts/blocks/customcardinal/garland.js
+++ source/kubejs/dingusprimecontentpack_scripts/startup_scripts/blocks/customcardinal/garland.js
@@ -1,3 +1,3 @@
 onEvent('block.registry', event => {
-	event.create('garland', 'customcardinal').displayName('Garland').material('wood').defaultCutout().notSolid().noValidSpawns(true).suffocating(false).viewBlocking(false).redstoneConductor(false)
+	event.create('garland', 'customcardinal').displayName('Garland').material('wood').defaultCutout().notSolid().noValidSpawns(true).suffocating(false).viewBlocking(false).redstoneConductor(false).box(0, 7, 14, 16, 9, 16, true)
 })
